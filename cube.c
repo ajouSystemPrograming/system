3b53f69123c6d929f137e754a599ce8339345fe5
unknown <asdfqwer1234@ajou.ac.kr>
2024-11-21 21:37:33 +0900

    serve

---
+++ cube.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+typedef unsigned long long ull;
+
+int i0, i1, i2, t0, t1, t2;
+ull mask[24];
+void init()
+{
+    mask[0]=1;
+    for(i0 = 0; i0 < 24; i0++)
+        mask[i0] = mask[i0-1]*6;
+}
+ull X(ull l0)
+{
+    long l1 = (l0/mask[5])%6, l2 = (l0/mask[7])%6, l3, l4;
+    l0-=l1*mask[5]; l0-=l2*mask[7];
+    l0+=(l3=(l0/mask[17])%6)*mask[5]; l0+=(l4=(l0/mask[19])%6)*mask[7];
+    l0-=l3*mask[17]; l0-=l4*mask[19];
+    l0+=(l3=(l0/mask[14])%6)*mask[17]; l0+=(l4=(l0/mask[12])%6)*mask[19];
+    l0-=l3*mask[14]; l0-=l4*mask[12];
+    l0+=(l3=(l0/mask[23])%6)*mask[12]; l0+=(l4=(l0/mask[21])%6)*mask[14];
+    l0-=l3*mask[23]; l0-=l4*mask[21];
+    l0+=l1*mask[21]; l0+=l2*mask[23];
+    //
+    l1 = (l0/mask[8])%6; l0-=l1*mask[8];
+    l0+=(l2=(l0/mask[9])%6)*mask[8]; l0-=l2*mask[9];
+    l0+=(l2=(l0/mask[11])%6)*mask[9]; l0-=l2*mask[11];
+    l0+=(l2=(l0/mask[10])%6)*mask[11]; l0-=l2*mask[10];
+    l0+=l1*mask[10];
+    return l0;
+}
+ull Y(ull l0)
+{
+    long l1 = (l0/mask[0])%6, l2 = (l0/mask[1])%6, l3, l4;
+    l0-=l1*mask[0]; l0-=l2*mask[1];
+    l0+=(l3=(l0/mask[12])%6)*mask[0]; l0+=(l4=(l0/mask[13])%6)*mask[1];
+    l0-=l3*mask[12]; l0-=l4*mask[13];
+    l0+=(l3=(l0/mask[8])%6)*mask[12]; l0+=(l4=(l0/mask[9])%6)*mask[13];
+    l0-=l3*mask[8]; l0-=l4*mask[9];
+    l0+=(l3=(l0/mask[4])%6)*mask[8]; l0+=(l4=(l0/mask[5])%6)*mask[9];
+    l0-=l3*mask[4]; l0-=l4*mask[5];
+    l0+=l1*mask[4]; l0+=l2*mask[5];
+    //
+    l1 = (l0/mask[16])%6; l0-=l1*mask[16];
+    l0+=(l2=(l0/mask[17])%6)*mask[16]; l0-=l2*mask[17];
+    l0+=(l2=(l0/mask[19])%6)*mask[17]; l0-=l2*mask[19];
+    l0+=(l2=(l0/mask[18])%6)*mask[19]; l0-=l2*mask[18];
+    l0+=l1*mask[18];
+    return l0;
+}
+ull Z(ull l0)
+{
+    long l1 = (l0/mask[1])%6, l2 = (l0/mask[3])%6, l3, l4;
+    l0-=l1*mask[1]; l0-=l2*mask[3];
+    l0+=(l3=(l0/mask[19])%6)*mask[1]; l0+=(l4=(l0/mask[18])%6)*mask[3];
+    l0-=l3*mask[19]; l0-=l4*mask[18];
+    l0+=(l3=(l0/mask[10])%6)*mask[19]; l0+=(l4=(l0/mask[8])%6)*mask[18];
+    l0-=l3*mask[10]; l0-=l4*mask[8];
+    l0+=(l3=(l0/mask[20])%6)*mask[10]; l0+=(l4=(l0/mask[21])%6)*mask[8];
+    l0-=l3*mask[20]; l0-=l4*mask[21];
+    l0+=l1*mask[20]; l0+=l2*mask[21];
+    //
+    l1 = (l0/mask[4])%6; l0-=l1*mask[4];
+    l0+=(l2=(l0/mask[5])%6)*mask[4]; l0-=l2*mask[5];
+    l0+=(l2=(l0/mask[7])%6)*mask[5]; l0-=l2*mask[7];
+    l0+=(l2=(l0/mask[6])%6)*mask[7]; l0-=l2*mask[6];
+    l0+=l1*mask[6];
+    return l0;
+}
+int main()
+{
+    long l0, l1;
+    init();
+    //input
+    l1 = X(l0);
+    l1 = Y(l0);
+    l1 = Z(l0);
+    //output
+    return 0;
+}
