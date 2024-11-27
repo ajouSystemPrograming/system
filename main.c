f7821257b46c71f481033cd3e538dbfb07dfa625
unknown <asdfqwer1234@ajou.ac.kr>
2024-11-27 22:16:27 +0900

    메인 조이스틱 입력

--- main.c
+++ main.c
@@ -16,43 +16,43 @@
 
 //test========================================================
 char cube[13][17]={
-    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
-    {7,7,7,7,7,4,7,4,7,7,7,7,7,7,7,7,7},
-    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
-    {7,7,7,7,7,4,7,4,7,7,7,7,7,7,7,7,7},
-    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
-    {7,0,7,0,7,1,7,1,7,2,7,2,7,3,7,3,7},
-    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
-    {7,0,7,0,7,1,7,1,7,2,7,2,7,3,7,3,7},
-    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
-    {7,7,7,7,7,5,7,5,7,7,7,7,7,7,7,7,7},
-    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
-    {7,7,7,7,7,5,7,5,7,7,7,7,7,7,7,7,7},
-    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7}
-};
+        {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+        {7,7,7,7,7,4,7,4,7,7,7,7,7,7,7,7,7},
+        {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+        {7,7,7,7,7,4,7,4,7,7,7,7,7,7,7,7,7},
+        {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+        {7,0,7,0,7,1,7,1,7,2,7,2,7,3,7,3,7},
+        {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+        {7,0,7,0,7,1,7,1,7,2,7,2,7,3,7,3,7},
+        {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+        {7,7,7,7,7,5,7,5,7,7,7,7,7,7,7,7,7},
+        {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+        {7,7,7,7,7,5,7,5,7,7,7,7,7,7,7,7,7},
+        {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7}
+        };
 //==========================================================test
 
 //
 #define SPIN 14
 #define SOUT 15
-#define UPPIN 17
-#define UPOUT 27
+#define UPPIN 19
+#define UPOUT 26
 #define DOWNPIN 20
 #define DOWNOUT 21
 #define LEFTPIN 6
 #define LEFTOUT 13
-#define RIGHTPIN 19
-#define RIGHTOUT 26
+#define RIGHTPIN 2
+#define RIGHTOUT 3
 
 char* tochar = "RGBYOW* ";
-int move[4][2] = {{-1,0},{1,0},{0,-1},{0,-1}};
+int move[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
 int frame[4][2] = {{1,1},{1,-1},{-1,1},{-1,-1}};
 void printCube(int r, int c)
 {
     int i0,i1;
     for(i0 = 0; i0 < 4; i0++)
         cube[r+frame[i0][0]][c+frame[i0][1]]=6;
-    
+
     for(i0 = 0; i0 < 13; i0++)
     {
         for(i1 = 0; i1 < 17; i1++)
@@ -109,7 +109,7 @@ static int GPIODirection(int pin, int dir) {
     snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
     fd = open(path, O_WRONLY);
     if (-1 == fd) {
-        fprintf(stderr, "Failed to open gpio direction for writing!\n");
+        fprintf(stderr, "Failed to open gpio%d direction for writing!\n", pin);
         return (-1);
     }
 
@@ -130,7 +130,7 @@ static int GPIORead(int pin) {
     snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
     fd = open(path, O_RDONLY);
     if (-1 == fd) {
-        fprintf(stderr, "Failed to open gpio value for reading!\n");
+        fprintf(stderr, "Failed to open gpio%d value for reading!\n", pin);
         return (-1);
     }
 
@@ -153,7 +153,7 @@ static int GPIOWrite(int pin, int value) {
     snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
     fd = open(path, O_WRONLY);
     if (-1 == fd) {
-        fprintf(stderr, "Failed to open gpio value for writing!\n");
+        fprintf(stderr, "Failed to open gpio%d value for writing!\n", pin);
         return (-1);
     }
 
@@ -168,12 +168,12 @@ static int GPIOWrite(int pin, int value) {
 //int len = 8;
 //int usePinNum[100] = {UPIN,UPOUT,DOWNIN,DOWNOUT,LEFTIN,LEFTOUT,RIGHTIN,RIGHTOUT};
 static int init(){
-    if (GPIOExport(SOUT) == -1 || GPIOExport(SPIN)) {
-        return 1;
-    }
-    if (GPIODirection(SOUT, OUT) == -1 || GPIODirection(SPIN, IN) == -1) {
-        return 2;
-    }
+//    if (GPIOExport(SOUT) == -1 || GPIOExport(SPIN)) {
+//        return 1;
+//    }
+//    if (GPIODirection(SOUT, OUT) == -1 || GPIODirection(SPIN, IN) == -1) {
+//        return 2;
+//    }
     if (GPIOExport(UPOUT) == -1 || GPIOExport(UPPIN)) {
         return 1;
     }
@@ -199,20 +199,20 @@ static int init(){
         return 2;
     }
 
-    printCube(r, c);
+    printCube(5, 1);
     return 0;
 }
 static int end(){
-    if (GPIOUnexport(SOUT) == -1 || GPIOUnexport(SPIN) == -1) {
-        return 4;
-    }
+//    if (GPIOUnexport(SOUT) == -1 || GPIOUnexport(SPIN) == -1) {
+//        return 4;
+//    }
     if (GPIOUnexport(UPOUT) == -1 || GPIOUnexport(UPPIN) == -1) {
         return 4;
     }
     if (GPIOUnexport(DOWNOUT) == -1 || GPIOUnexport(DOWNPIN) == -1) {
         return 4;
     }
-    if (GPIOUnexport(LEFTOUT) == -1 || GPIOUnexport(RIGHTPIN) == -1) {
+    if (GPIOUnexport(LEFTOUT) == -1 || GPIOUnexport(LEFTPIN) == -1) {
         return 4;
     }
     if (GPIOUnexport(RIGHTOUT) == -1 || GPIOUnexport(RIGHTPIN) == -1) {
@@ -223,61 +223,62 @@ static int end(){
 }
 
 static int ButtonInput(){
-    int state = 1;
-    int prev_state = 1;
-    int light = 0;
-    
+
     int buttonState = 0;
     if (GPIOWrite(UPOUT, 1) == -1) {
         return 3;
     }
-    buttonState += GPIORead(UPIN);
+    buttonState += (1^GPIORead(UPPIN));
+
     if (GPIOWrite(DOWNOUT, 1) == -1) {
         return 3;
     }
-    buttonState += GPIORead(DOWNIN)<<1;
+    buttonState += (1^GPIORead(DOWNPIN))<<1;
+
     if (GPIOWrite(LEFTOUT, 1) == -1) {
         return 3;
     }
-    buttonState += GPIORead(RIGHTIN)<<2;
-    if (GPIOWrite(UPOUT, 1) == -1) {
+    buttonState += (1^GPIORead(LEFTPIN))<<2;
+
+    if (GPIOWrite(RIGHTOUT, 1) == -1) {
         return 3;
     }
-    buttonState += GPIORead(RIGHTIN)<<3;
-    if (GPIOWrite(SOUT, 1) == -1) {
-        return 3;
-    }
-    buttonState += GPIORead(SIN)<<4;
-    printf("BUTTONSTATE: U:%d D:%d L:%d R:%d SELECT:%d\n", (buttonState,buttonState&(1<<1))>>1,(buttonState&(1<<2))>>2,(buttonState&(1<<3))>>3,(buttonState&(1<<4))>>4);
+    buttonState += (1^GPIORead(RIGHTPIN))<<3;
+//    if (GPIOWrite(SOUT, 1) == -1) {
+//        return 3;
+//    }
+    //buttonState += GPIORead(SPIN)<<4;
+    //printf("%d %d %d %d\n",GPIORead(UPPIN),GPIORead(DOWNPIN),GPIORead(LEFTPIN),GPIORead(RIGHTPIN));
+    //printf("BUTTONSTATE:%d U:%d D:%d L:%d R:%d SELECT:%d\n", buttonState, buttonState&1,(buttonState&(1<<1))>>1,(buttonState&(1<<2))>>2,(buttonState&(1<<3))>>3,(buttonState&(1<<4))>>4);
     return buttonState;
 }
 
-int main(int argc, char* argv[]) {
-    
-    if(!init())
-        return 1;
+int main() {
 
     int repeat = 100;
     int t0, pre, r = 5, c = 1;
-    while(repeat-->0)
+    int i0;
+    if(init())
+        return 1;
+    while(1)
     {
         t0 = ButtonInput();
-        if(t0&1<<3)
+        if(t0&(1<<4))
         {
-
+            break;
         }
         else if(t0)
         {
             for(i0 = 0; i0 < 4; i0++)
                 if((1<<i0)&t0){
-                    r += move[i0][0];
-                    c += move[i0][1];
+                    r += move[i0][0]<<1;
+                    c += move[i0][1]<<1;
                 }
             if(r<1) r = 1; else if(r>11) r = 11;
             if(c<1) c = 1; else if(c>15) c = 15;
             printCube(r, c);
         }
-        usleep(1000 * 1000);//1s
+        usleep(1000*1000);//1s
     }
 
     return end();
