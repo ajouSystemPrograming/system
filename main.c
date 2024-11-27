9d5a464fdda14dd6569a6e7aa43a131230bd4053
unknown <asdfqwer1234@ajou.ac.kr>
2024-11-27 16:41:58 +0900

    메인파이 전개도 이동 추가

--- main.c
+++ main.c
@@ -14,6 +14,24 @@
 #define VALUE_MAX 40
 #define DIRECTION_MAX 40
 
+//test========================================================
+char cube[13][17]={
+    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+    {7,7,7,7,7,4,7,4,7,7,7,7,7,7,7,7,7},
+    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+    {7,7,7,7,7,4,7,4,7,7,7,7,7,7,7,7,7},
+    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+    {7,0,7,0,7,1,7,1,7,2,7,2,7,3,7,3,7},
+    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+    {7,0,7,0,7,1,7,1,7,2,7,2,7,3,7,3,7},
+    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+    {7,7,7,7,7,5,7,5,7,7,7,7,7,7,7,7,7},
+    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7},
+    {7,7,7,7,7,5,7,5,7,7,7,7,7,7,7,7,7},
+    {7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7}
+};
+//==========================================================test
+
 //
 #define UPPIN 17
 #define UPOUT 27
@@ -24,8 +42,26 @@
 #define RIGHTPIN 19
 #define RIGHTOUT 26
 
-int buttonState = 0;
+char* tochar = "RGBYOW* ";
+int move[4][2] = {{-1,0},{1,0},{0,-1},{0,-1}};
+int frame[4][2] = {{1,1},{1,-1},{-1,1},{-1,-1}};
+void printCube(int r, int c)
+{
+    int i0,i1;
+    for(i0 = 0; i0 < 4; i0++)
+        cube[r+frame[i0][0]][c+frame[i0][1]]=6;
+    
+    for(i0 = 0; i0 < 13; i0++)
+    {
+        for(i1 = 0; i1 < 17; i1++)
+            printf("%c",tochar[cube[i0][i1]]);
+        printf("\n");
+    }
+    for(i0 = 0; i0 < 4; i0++)
+        cube[r+frame[i0][0]][c+frame[i0][1]]=7;
+}
 //
+
 
 static int GPIOExport(int pin) {
     #define BUFFER_MAX 3
@@ -127,11 +163,9 @@ static int GPIOWrite(int pin, int value) {
     close(fd);
     return (0);
 }
-//
 //int len = 8;
 //int usePinNum[100] = {UPIN,UPOUT,DOWNIN,DOWNOUT,LEFTIN,LEFTOUT,RIGHTIN,RIGHTOUT};
-static int init()
-{
+static int init(){
     if (GPIOExport(UPOUT) == -1 || GPIOExport(UPPIN)) {
         return 1;
     }
@@ -157,11 +191,10 @@ static int init()
         return 2;
     }
 
+    printCube(r, c);
     return 0;
 }
-static int end()
-{
-
+static int end(){
     if (GPIOUnexport(UPOUT) == -1 || GPIOUnexport(UPPIN) == -1) {
         return 4;
     }
@@ -179,34 +212,48 @@ static int end()
 }
 
 static int ButtonInput(){
-    int repeat = 100;
     int state = 1;
     int prev_state = 1;
     int light = 0;
-
-    do {
-        if (GPIOWrite(UPOUT2, 1) == -1) {
-            return 3;
-        }
-        buttonState=0;
-        buttonState += GPIORead(UPIN);
-        buttonState += GPIORead(DOWNIN)<<1;
-        buttonState += GPIORead(LEFTIN)<<2;
-        buttonState += GPIORead(RIGHTIN)<<3;
-        printf("BUTTONSTATE: U:%d D:%d L:%d R:%d\n", buttonState,buttonState&(1),buttonState&(1<<2),buttonState&(1<<3));
-        usleep(1000 * 1000);//1s
-    } while (repeat--);
+    
+    int buttonState = 0;
+    if (GPIOWrite(UPOUT2, 1) == -1) {
+        return 3;
+    }
+    buttonState += GPIORead(UPIN);
+    buttonState += GPIORead(DOWNIN)<<1;
+    buttonState += GPIORead(LEFTIN)<<2;
+    buttonState += GPIORead(RIGHTIN)<<3;
+    printf("BUTTONSTATE: U:%d D:%d L:%d R:%d\n", buttonState,buttonState&(1),buttonState&(1<<2),buttonState&(1<<3));
+    return buttonState;
 }
-//
 
 int main(int argc, char* argv[]) {
     
     if(!init())
         return 1;
 
-    ButtonInput();
-
-
+    int repeat = 100;
+    int t0, pre, r = 5, c = 1;
+    while(repeat-->0)
+    {
+        t0 = ButtonInput();
+        for(i0 = 0; i0 < 4; i0++)
+            if((1<<i0)&t0!=0){
+                r += move[i0][0];
+                c += move[i0][1];
+            }
+        if(r<1)
+            r = 1;
+        else if(r>11)
+            r = 11;
+        if(c<1)
+            c = 1;
+        else if(c>15)
+            c = 15;
+        printCube(r, c);
+        usleep(1000 * 1000);//1s
+    }
 
     return end();
 }
